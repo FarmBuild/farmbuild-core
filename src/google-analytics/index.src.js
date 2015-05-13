f32aa704e2d49c52597629381a25efa91e88462e
hxg <hiroki.gota@spatialvision.com.au>
2015-05-13 12:43:44 +1000

    added tracker name

--- src/google-analytics/index.src.js
+++ src/google-analytics/index.src.js
@@ -16,19 +16,22 @@ angular.module('farmbuild.core')
   .factory('googleAnalytics',
     function ($log, validations) {
         var googleAnalytics = {},
-            _isDefined = validations.isDefined;
+            _isDefined = validations.isDefined,
+          trackerName = 'farmbuildTracker';
 
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-53478356-1', 'auto');
+        ga('create', 'UA-53478356-1', 'auto', {'name': name});
         ga('send', 'pageview');
 
-        googleAnalytics.track = function(api, clientName) {
-            if(!_isDefined(api)) {
-                $log.error('googleAnalytics.track api must be provided.' +
+
+
+        googleAnalytics.track = function(apiName, clientName) {
+            if(!_isDefined(apiName)) {
+                $log.error('googleAnalytics.track apiName must be provided.' +
                 ' Please specify you API name.');
                 return;
             }
@@ -38,10 +41,10 @@ angular.module('farmbuild.core')
                 return;
             }
 
-            $log.info('googleAnalytics.track api: %s, clientName: %s', api, clientName);
+            $log.info('googleAnalytics.track apiName: %s, clientName: %s', apiName, clientName);
 
             ga('send', 'pageview', {
-                'dimension4':  api,
+                'dimension4':  apiName,
                 'dimension5': clientName
             });
         }
